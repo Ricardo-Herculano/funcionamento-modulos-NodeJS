2a7e97a2062d424870a4a5131dd5d4cf3c80b580
Ricardo Herculano <98854473+Ricardo-Herculano@users.noreply.github.com>
2022-03-26 10:49:43 -0300

    Fundamentos de uma aplicação no Javascript
    Entendendo os fundamentos de uma aplicação no Javascript

---
+++ app/calculadora.js
@@ -0,0 +1,6 @@
+var soma = require("../soma");
+var dividir = require("../dividir");
+var multiplicar = require("../multiplicar");
+var subtrair = require("../subtrair");
+
+console.log (subtrair(2,2));
